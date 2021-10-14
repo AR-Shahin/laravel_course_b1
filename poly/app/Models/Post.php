e861ed72914c83f2d9f29f3a5d23d50a17484976
AR Shahin <mdshahinmije96@gmail.com>
2021-10-14 22:07:33 +0600

    one to many poly

---
+++ poly/app/Models/Post.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+
+    public function comments()
+    {
+        return $this->morphMany(Comment::class, 'commentable');
+    }
+
+    public function latestComment()
+    {
+        return $this->morphOne(Comment::class, 'commentable')->latestOfMany();
+    }
+
+    public function oldestComment()
+    {
+        return $this->morphOne(Comment::class, 'commentable')->oldestOfMany();
+    }
+}
--- poly/routes/web.php
+++ poly/routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
-use App\Models\Admin;
 use App\Models\Info;
+use App\Models\Post;
 use App\Models\User;
+use App\Models\Admin;
+use App\Models\Comment;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 
@@ -18,9 +21,31 @@ Route::get('/', function () {
     //     'address' => 'Chandpur'
     // ]);
 
-    return Admin::with('info')->find(2);
+    // return Admin::with('info')->find(2);
 
-    return $info = Info::find(2)->subject;
+    // return $info = Info::find(1)->subject->getTable();
     // dd($info);
+
+    // $post = Product::create([
+    //     'name' => 'Post 3'
+    // ]);
+    // for ($i = 1; $i <= 3; $i++) {
+    //     $post->comments()->create([
+    //         'title' => "Comment  Product $i"
+    //     ]);
+    // }
+
+    return Product::with('oldestComment')->find(1);
+
+
+    return Comment::find(8)->subject->getTable();
+
+
+
+
+
+
+
+
     return view('welcome');
 });
